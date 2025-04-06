fdc9eb5f4ed0f6bc8600cf5414191c64ccfd19c6
Kyle <kylestanley733@gmail.com>
2025-04-06 11:59:37 -0500

    Added ability to structure messages and read them

--- message.go
+++ message.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"bytes"
 	"fmt"
 	"net/http"
 	"bufio"
 	"os"
 	"strings"
-	"io"
+	"errors"
+	//"io"
 )
 var (
 	token     = readConf("config.conf", "TOKEN")
 	channelID = readConf("config.conf", "CHANNEL_ID")
 	apiURL    = "https://discord.com/api/v10/channels/" + channelID + "/messages"
 )
+
+
+type Author struct {
+	Username string `json:"username"`
+	ID       string `json:"id"`
+	Bot      bool   `json:"bot"`
+}
+
+type Message struct {
+	ID      string `json:"id"`
+	Content string `json:"content"`
+	Author  Author `json:"author"`
+}
 
 func check(e error) {
     if e != nil {
@@ -53,7 +68,7 @@ func postMessage(message string) {
 	fmt.Println("Message sent! Status:", resp.Status)
 }
 
-func readMessages() {
+func readMessages() []Message {
 	
 	req, _ := http.NewRequest("GET", apiURL, nil)
 	req.Header.Set("Authorization", "Bot "+token)
@@ -63,22 +78,42 @@ func readMessages() {
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return []Message{}
 	}
 	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
+	var messages []Message
+	err = json.NewDecoder(resp.Body).Decode(&messages)
 	if err != nil {
-	    fmt.Println("Error reading response:", err)
-	    return
+		fmt.Println("Decode error:", err)
+		return []Message{}
 	}
-	fmt.Println(string(body))
-	fmt.Println("Message sent! Status:", resp.Status)
+	return messages
+}
+func searchMessages(messageID string, messages []Message) (Message, error) {
+	for _, message := range messages {
+		if message.ID  == messageID {
+			return message, nil
+		}
+	}
+	return Message{}, errors.New("message not found: " + messageID)
 }
 
+func connectMessagesAndCreateResponse(m1 Message, m2 Message) string {
+	return fmt.Sprintf("Of course I know why you said '%s' %s.  It's because you were inextricably entangled in a quantum field with %s the moment they uttered '%s', or was it the other way around?",m1.Content, m1.Author.Username, m2.Author.Username, m2.Content)
+}
 
 func main() {
-	message := `{"content": "Hello, Discord!"}`
-	readMessages()
+	messagesRead := readMessages()
+	m1, err1 := searchMessages(readConf("config.conf", "MESSAGE_ID1"), messagesRead)
+	if err1 != nil {
+		panic(err1)
+	}
+	m2, err2 := searchMessages(readConf("config.conf", "MESSAGE_ID2"), messagesRead)
+	if err2 != nil {
+		panic(err2)
+	}
+	messageContent := connectMessagesAndCreateResponse(m1, m2)
+	message := fmt.Sprintf(`{"content": "%s"}`, messageContent)
+	fmt.Println(message)
 	postMessage(message)
 }
